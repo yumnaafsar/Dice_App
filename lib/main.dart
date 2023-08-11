2da25d4de460ba137a78736f85c46616642df506
yumnaafsar <yumnaafsar14@gmail.com>
2023-08-11 19:12:12 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -26,19 +26,19 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DicePage extends StatelessWidget {
-
-  var leftDiceNumnber=1;
-  var RightDiceNumnber=1;
-
-  void changeDice()
-  {
-
-  }
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+   var leftDiceNumnber=1;
+  var RightDiceNumnber=1;
+  @override
   Widget build(BuildContext context) {
-    return Center(
+    return  Center(
       child: Row(
         children: [
           // Expanded(
@@ -56,7 +56,12 @@ class DicePage extends StatelessWidget {
               padding: const EdgeInsets.all(16.0),
               child: TextButton(
                 onPressed: (){
-                   
+                   RightDiceNumnber=Random().nextInt(6)+1;
+                  setState(() {
+                    
+                  });
+                  
+            
                    print('right button got clicked');
                 },
                 child: Image.asset('images/dice$RightDiceNumnber.png')),
@@ -69,7 +74,11 @@ class DicePage extends StatelessWidget {
               child: TextButton(
                 onPressed: (){
                   
-                  RightDiceNumnber=Random().nextInt(6)+1;
+                  leftDiceNumnber=Random().nextInt(6)+1;
+                  setState(() {
+                    
+                  });
+                  
                   print('left button got clicked');
                 },
                 child: Image.asset('images/dice$leftDiceNumnber.png')),
@@ -80,3 +89,59 @@ class DicePage extends StatelessWidget {
     );
   }
 }
+
+// class DicePage extends StatelessWidget {
+
+//   var leftDiceNumnber=1;
+//   var RightDiceNumnber=1;
+
+//   void changeDice()
+//   {
+
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Row(
+//         children: [
+//           // Expanded(
+//           //   flex: 1,
+//           //   child: Image(image: AssetImage('images/dice1.png'),)
+//           //   ),
+//           //    Expanded(
+//           //     flex: 2,
+//           //   child: Image(image: AssetImage('images/dice2.png'),)
+//           //   ),
+    
+//             Expanded(
+//             // flex: 1,
+//             child: Padding(
+//               padding: const EdgeInsets.all(16.0),
+//               child: TextButton(
+//                 onPressed: (){
+            
+//                    print('right button got clicked');
+//                 },
+//                 child: Image.asset('images/dice$RightDiceNumnber.png')),
+//             )
+//             ),
+//              Expanded(
+//               // flex: 2,
+//             child: Padding(
+//               padding: const EdgeInsets.all(16.0),
+//               child: TextButton(
+//                 onPressed: (){
+                  
+//                   RightDiceNumnber=Random().nextInt(6)+1;
+                  
+//                   print('left button got clicked');
+//                 },
+//                 child: Image.asset('images/dice$leftDiceNumnber.png')),
+//             )
+//             )
+//         ],
+//       ),
+//     );
+//   }
+// }
